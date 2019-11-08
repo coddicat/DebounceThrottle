265eaca90555a5e6a672a46fbc635ce18960e644
Rodion Shlomo Solomonyk <shlomo.rodion@tipalti.com>
2019-11-08 10:45:39 +0200

    publish DebounceThrottle to GitHub

---
+++ DebouneThrottle/DebounceDispatcher.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DebouneThrottle
+{
+    /// <summary>
+    /// The debounce dispatcher delays the invoking of Action until the calls stopped for a predetermined interval
+    /// </summary>
+    public class DebounceDispatcher : DebounceDispatcher<bool>
+    {
+        #region --- ctor ---
+        /// <summary>
+        /// The debounce dispatcher delays the invoking of Action until the calls stopped for a predetermined interval
+        /// </summary>
+        /// <param name="interval">The interval in milliseconds that must elapse after the last call so that a Action to be invoked</param>
+        public DebounceDispatcher(int interval) : base(interval)
+        {
+        }
+        #endregion
+
+        #region --- public methods ---
+        /// <summary>
+        /// The call of the Action that will be invoked after waiting for the time interval if there are no more calls in this interval
+        /// </summary>
+        /// <param name="action">Action that will be invoked after waiting for the time interval</param>
+        /// <returns>Task that will complete when Action will be invoked</returns>
+        public Task DebounceAsync(Func<Task> action)
+        {
+            return base.DebounceAsync(async () =>
+            {
+                await action.Invoke();
+                return true;
+            });
+        }
+        /// <summary>
+        /// The call of the Action that will be invoked after waiting for the time interval if there are no more calls in this interval
+        /// </summary>
+        /// <param name="action">Action that will be invoked after waiting for the time interval</param>
+        public void Debounce(Action action)
+        {
+            Func<Task<bool>> actionAsync = () => Task.Run(() =>
+            {
+                action.Invoke();
+                return true;
+            });
+
+            DebounceAsync(actionAsync);
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// The debounce dispatcher delays the invoking of Function until the calls stopped for a predetermined interval
+    /// </summary>
+    /// <typeparam name="T">The return Type of the Tasks. All tasks will return the same value if the invoking occurs once</typeparam>
+    public class DebounceDispatcher<T>
+    {
+        #region --- private fields ---
+        private DateTime lastInvokeTime;
+        private readonly int interval;
+        private Func<Task<T>> functToInvoke;
+        private object locker = new object();
+        private bool busy;
+        private Task<T> waitingTask;
+        #endregion
+
+        #region --- ctor ---
+        /// <summary>
+        /// The debounce dispatcher delays the invoking of Function until the calls stopped for a predetermined interval
+        /// </summary>
+        /// <param name="interval">The interval in milliseconds that must elapse after the last call so that a Function to be invoked</param>
+        public DebounceDispatcher(int interval)
+        {
+            this.interval = interval;
+        }
+        #endregion
+
+        #region --- public methods ---
+        /// <summary>
+        /// The call of the Function that will be invoked after waiting for the time interval if there are no more calls in this interval
+        /// </summary>
+        /// <param name="functToInvoke">Function that will be invoked after waiting for the time interval</param>
+        /// <returns>Task with a result that will complete when Function will be invoked. All tasks will return the same value if the invoking occurs once</returns>
+        public Task<T> DebounceAsync(Func<Task<T>> functToInvoke)
+        {
+            lock (locker)
+            {
+                this.functToInvoke = functToInvoke;
+                this.lastInvokeTime = DateTime.UtcNow;
+                if (busy)
+                {
+                    return waitingTask;
+                }
+
+                busy = true;
+                waitingTask = Task.Run(() =>
+                {
+                    do
+                    {
+                        int delay = (int)(interval - (DateTime.UtcNow - lastInvokeTime).TotalMilliseconds);
+                        Task.Delay(delay).Wait();
+                    } while ((DateTime.UtcNow - lastInvokeTime).TotalMilliseconds < interval);
+
+                    T res;
+                    try
+                    {
+                        res = this.functToInvoke.Invoke().Result;
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                    finally
+                    {
+                        lock (locker)
+                        {
+                            busy = false;
+                        }
+                    }
+
+                    return res;
+                });
+                return waitingTask;
+            }
+        }
+        #endregion
+    }
+}
---
+++ DebouneThrottle/ThrottleDispatcher.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DebouneThrottle
+{
+    /// <summary>
+    /// The Throttle dispatcher provides one Action invoking at a specific time interval
+    /// </summary>
+    public class ThrottleDispatcher : ThrottleDispatcher<bool>
+    {
+        #region --- ctor ---
+        /// <summary>
+        /// The Throttle dispatcher provides one Action invoking at a specific time interval
+        /// </summary>
+        /// <param name="interval">The time interval when should be only one invoking</param>
+        /// <param name="intervalFromStart">true - Countdown of the time interval from the beginning of the Action invoking. false - Countdown of the time interval from the complete of the Action invoking</param>
+        /// <param name="resetIntervalOnException">true - if an error occurs, ignore call and do not wait for the time interval for next call</param>
+        public ThrottleDispatcher(int interval, bool intervalFromStart = false, bool resetIntervalOnException = false)
+            : base(interval, intervalFromStart, resetIntervalOnException)
+        {
+        }
+        #endregion
+
+        #region --- public methods ---
+        /// <summary>
+        /// Call the Action that will be invoked if it was the only one at a specific time interval 
+        /// </summary>
+        /// <param name="action">Action that will be invoked</param>
+        /// <returns>Task that will complete when one of called Action will be invoked</returns>
+        public Task ThrottleAsync(Func<Task> action)
+        {
+            return base.ThrottleAsync(() =>
+            {
+                action.Invoke().Wait();
+                return Task.FromResult(true);
+            });
+        }
+        /// <summary>
+        /// Call the Action that will be invoked if it was the only one at a specific time interval
+        /// </summary>
+        /// <param name="action">Action that will be invoked</param>
+        public void Throttle(Action action)
+        {
+            Func<Task<bool>> actionAsync = () => Task.Run(() =>
+            {
+                action.Invoke();
+                return true;
+            });
+
+            ThrottleAsync(actionAsync);
+        }
+        #endregion
+    }
+    /// <summary>
+    /// The Throttle dispatcher provides one Function invoking at a specific time interval
+    /// </summary>
+    /// <typeparam name="T">The return Type of the Tasks. All tasks will return the same value if the invoking occurs once</typeparam>
+    public class ThrottleDispatcher<T>
+    {
+        #region --- private fields ---
+        private Func<Task<T>> functToInvoke;
+        private readonly int interval;
+        private readonly bool isIntervalSinceInvokeTime;
+        private readonly bool resetIntervalOnException;
+        private readonly object locker = new object();
+        private bool busy;
+        private bool waiting;
+        private Task<T> waitingTask;
+        private Task intervalTask;
+        private DateTime? invokeStartTime;
+        #endregion
+
+        #region --- ctor ---
+        /// <summary>
+        /// The Throttle dispatcher provides one Function invoking at a specific time interval
+        /// </summary>
+        /// <param name="interval">The time interval when should be only one invoking</param>
+        /// <param name="intervalFromStart">true - Countdown of the time interval from the beginning of the Function invoking. false - Countdown of the time interval from the complete of the Function invoking</param>
+        /// <param name="resetIntervalOnException">true - if an error occurs, ignore call and do not wait for the time interval for next call</param>
+        public ThrottleDispatcher(int interval, bool isIntervalSinceInvokeTime = false, bool resetIntervalOnException = false)
+        {
+            this.interval = interval;
+            this.isIntervalSinceInvokeTime = isIntervalSinceInvokeTime;
+            this.resetIntervalOnException = resetIntervalOnException;
+        }
+        #endregion
+
+        #region --- public methods ---
+        /// <summary>
+        /// Call the Function that will be invoked if it was the only one at a specific time interval
+        /// </summary>
+        /// <param name="functToInvoke">Function that will be invoked</param>
+        /// <returns>Task with a result that will complete when one of called Function will be invoked</returns>
+        public Task<T> ThrottleAsync(Func<Task<T>> functToInvoke)
+        {
+            lock (locker)
+            {
+                this.functToInvoke = functToInvoke;
+
+                if (busy)
+                {
+                    return GetWaitingTask();
+                }
+
+                Task<T> actionTask = GetActionTask();
+
+                intervalTask = Task.Run(() => ProcessInterval(actionTask));
+
+                return actionTask;
+            }
+        }
+        #endregion
+
+        #region --- private methods ---
+        private Task<T> GetActionTask()
+        {
+            busy = true;
+            invokeStartTime = DateTime.UtcNow;
+            Task<T> actionTask = this.functToInvoke.Invoke();
+            return actionTask;
+        }
+
+        private Task<T> GetWaitingTask()
+        {
+            if (waiting)
+            {
+                return waitingTask;
+            }
+
+            waiting = true;
+            waitingTask = Task.Run(() => ProcessWaitingTask());
+
+            return waitingTask;
+        }
+
+        private Task<T> ProcessWaitingTask()
+        {
+            intervalTask.Wait();
+            lock (locker)
+            {
+                waiting = false;
+                return ThrottleAsync(this.functToInvoke);
+            }
+        }
+
+        private void ProcessInterval(Task actionTask)
+        {
+            try
+            {
+                actionTask.Wait();
+                DelayToNextProcess();
+            }
+            catch
+            {
+                if (!resetIntervalOnException)
+                {
+                    DelayToNextProcess();
+                }
+            }
+            finally
+            {
+                lock (locker)
+                {
+                    busy = false;
+                }
+            }
+        }
+
+        private void DelayToNextProcess()
+        {
+            int delay = interval;
+            if (isIntervalSinceInvokeTime && invokeStartTime.HasValue)
+            {
+                delay = (int)(interval - (DateTime.UtcNow - invokeStartTime.Value).TotalMilliseconds);
+            }
+            if (delay > 0)
+            {
+                //Thread.Sleep(delay);
+                Task.Delay(delay).Wait();
+            }
+        }
+        #endregion
+    }
+}
