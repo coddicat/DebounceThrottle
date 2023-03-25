fbd318e86fcf5795af6c5149ca110a9bb7f57670
CoddiCat <32703004+coddicat@users.noreply.github.com>
2023-03-25 18:53:14 +0300

    revised

--- DebounceThrottle/DebounceDispatcher.cs
+++ DebounceThrottle/DebounceDispatcher.cs
@@ -1,128 +1,52 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DebounceThrottle
 {
     /// <summary>
-    /// The debounce dispatcher delays the invoking of Action until the calls stopped for a predetermined interval
+    /// The Debounce dispatcher delays the invocation of an action until a predetermined interval has elapsed since the last call. This ensures that the action is only invoked once after the calls have stopped for the specified duration.
     /// </summary>
     public class DebounceDispatcher : DebounceDispatcher<bool>
     {
-        #region --- ctor ---
         /// <summary>
-        /// The debounce dispatcher delays the invoking of Action until the calls stopped for a predetermined interval
+        /// Debouncing the execution of asynchronous tasks.
+        /// It ensures that a function is invoked only once within a specified interval, even if multiple invocations are requested.
         /// </summary>
-        /// <param name="interval">The interval in milliseconds that must elapse after the last call so that a Action to be invoked</param>
+        /// <param name="interval">The minimum interval in milliseconds between invocations of the debounced function.</param>
         public DebounceDispatcher(int interval) : base(interval)
         {
         }
-        #endregion
 
-        #region --- public methods ---
         /// <summary>
-        /// The call of the Action that will be invoked after waiting for the time interval if there are no more calls in this interval
+        /// Method manages the debouncing of the function invocation.
         /// </summary>
-        /// <param name="action">Action that will be invoked after waiting for the time interval</param>
-        /// <returns>Task that will complete when Action will be invoked</returns>
-        public Task DebounceAsync(Func<Task> action)
+        /// <param name="function">The function returns Task to be invoked asynchronously</param>
+        /// <param name="cancellationToken">An optional CancellationToken</param>
+        /// <returns>Returns Task to be executed with minimal delay</returns>
+        public Task DebounceAsync(Func<Task> function, CancellationToken cancellationToken = default)
         {
             return base.DebounceAsync(async () =>
             {
-                await action.Invoke();
+                await function.Invoke();
                 return true;
-            });
+            }, cancellationToken);
         }
+
         /// <summary>
-        /// The call of the Action that will be invoked after waiting for the time interval if there are no more calls in this interval
+        /// Method manages the debouncing of the function invocation.
         /// </summary>
-        /// <param name="action">Action that will be invoked after waiting for the time interval</param>
-        public void Debounce(Action action)
+        /// <param name="action">The action to be invoked</param>
+        /// <param name="cancellationToken">An optional CancellationToken</param>
+        public void Debounce(Action action, CancellationToken cancellationToken = default)
         {
             Func<Task<bool>> actionAsync = () => Task.Run(() =>
             {
                 action.Invoke();
                 return true;
-            });
+            }, cancellationToken);
 
-            DebounceAsync(actionAsync);
+            Task _ = DebounceAsync(actionAsync, cancellationToken);
         }
-        #endregion
-    }
-
-    /// <summary>
-    /// The debounce dispatcher delays the invoking of Function until the calls stopped for a predetermined interval
-    /// </summary>
-    /// <typeparam name="T">The return Type of the Tasks. All tasks will return the same value if the invoking occurs once</typeparam>
-    public class DebounceDispatcher<T>
-    {
-        #region --- private fields ---
-        private DateTime lastInvokeTime;
-        private readonly int interval;
-        private Func<Task<T>> functToInvoke;
-        private object locker = new object();
-        private bool busy;
-        private Task<T> waitingTask;
-        #endregion
-
-        #region --- ctor ---
-        /// <summary>
-        /// The debounce dispatcher delays the invoking of Function until the calls stopped for a predetermined interval
-        /// </summary>
-        /// <param name="interval">The interval in milliseconds that must elapse after the last call so that a Function to be invoked</param>
-        public DebounceDispatcher(int interval)
-        {
-            this.interval = interval;
-        }
-        #endregion
-
-        #region --- public methods ---
-        /// <summary>
-        /// The call of the Function that will be invoked after waiting for the time interval if there are no more calls in this interval
-        /// </summary>
-        /// <param name="functToInvoke">Function that will be invoked after waiting for the time interval</param>
-        /// <returns>Task with a result that will complete when Function will be invoked. All tasks will return the same value if the invoking occurs once</returns>
-        public Task<T> DebounceAsync(Func<Task<T>> functToInvoke)
-        {
-            lock (locker)
-            {
-                this.functToInvoke = functToInvoke;
-                this.lastInvokeTime = DateTime.UtcNow;
-                if (busy)
-                {
-                    return waitingTask;
-                }
-
-                busy = true;
-                waitingTask = Task.Run(() =>
-                {
-                    do
-                    {
-                        int delay = (int)(interval - (DateTime.UtcNow - lastInvokeTime).TotalMilliseconds);
-                        Task.Delay(delay).Wait();
-                    } while ((DateTime.UtcNow - lastInvokeTime).TotalMilliseconds < interval);
-
-                    T res;
-                    try
-                    {
-                        res = this.functToInvoke.Invoke().Result;
-                    }
-                    catch (Exception)
-                    {
-                        throw;
-                    }
-                    finally
-                    {
-                        lock (locker)
-                        {
-                            busy = false;
-                        }
-                    }
-
-                    return res;
-                });
-                return waitingTask;
-            }
-        }
-        #endregion
     }
 }
---
+++ Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using DebounceThrottle;
+
+bool stop = false;
+//trigger when to stop and exit
+var stopTask = Task.Run(() => { Console.ReadKey(true); stop = true; });
+
+var throttleDispatcher = new ThrottleDispatcher(1000);
+do
+{
+    //every iteration call dispatcher but the Action will be invoked only once in 1500 milliseconds (500 action work time + 1000 interval)
+    throttleDispatcher.Throttle(() =>
+    {
+        Console.WriteLine($"{DateTime.UtcNow.ToString("hh:mm:ss.fff")}");
+    });
+}
+while (!stop); //wait trigger to stop and exit
